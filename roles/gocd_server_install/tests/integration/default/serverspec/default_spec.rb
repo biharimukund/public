2eb8d8c978e9fd97b31a181fbe0bf41a1aaeb25b
Mukund Bihari <mukund_bihari@mckinsey.com>
2017-10-17 11:04:37 -0400

    modfied kitchen.yml to mention the path of default.yml

--- roles/gocd_server_install/tests/integration/default/serverspec/default_spec.rb
+++ roles/gocd_server_install/tests/integration/default/serverspec/default_spec.rb
@@ -2,10 +2,17 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe command('systemctl status go-server') do
+describe command('systemctl status artifactory.service') do
   its(:stdout) { should contain('running') }
 end
 
+describe file('/var/opt/jfrog/artifactory/etc/artifactory.lic') do
+  it { should be_file }
+end
+
+describe file('/var/opt/jfrog/artifactory/etc/binarystore.xml') do
+  it { should contain 'cluster-file-system' }
+end
 
 
 
